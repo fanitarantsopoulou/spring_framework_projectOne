315910227da87c07ed080ef69b9586da980fe305
Fani Tarantsopoulou <fanitarants@gmail.com>
2023-10-18 17:11:08 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         /*CPU coreI5 = new CPU(3.5f, "CoreI5");
         System.out.println(coreI5);*/
-        Computer hp250 = new Computer(15,
-                new CPU(1.5f, "core2DOU"),
-                new RAM(4, "DDOU")
+        Computer hp250 = new Computer(15.0,
+                new CPU(1.5, "core2DOU"),
+                new RAM(4.0, "DDOU")
         );
 
-        System.out.println(hp250);
+        System.out.println("The computer is an: hp 250.");
     }
 }
